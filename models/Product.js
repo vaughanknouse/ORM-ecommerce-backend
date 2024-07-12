155f447cf7360c700550cd3fca031232169bf185
Vaughan Knouse <vaughanknouse@gmail.com>
2024-07-12 12:35:44 -0400

    updated some code

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,32 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -97,3 +97,7 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
+
+
+
+
